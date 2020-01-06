9e8107021e4d1a4faeb10cd999cdaf3e4dfb3d7f
JeannaJ <jeanna.chandy99@gmail.com>
2020-01-05 19:53:47 -0800

    changes

--- manage.py
+++ manage.py
@@ -5,6 +5,10 @@ import sys
 
 
 def main():
+    os.environ['AWS_ACCESS_KEY_ID'] = 'AKIARDNEJAIQYLYQSD5K'
+    os.environ['AWS_SECRET_ACCESS_KEY'] = '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'
+    os.environ['AWS_STORAGE_BUCKET_NAME'] = 'postento'
+    
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'postentoApplication.settings')
     try:
         from django.core.management import execute_from_command_line
